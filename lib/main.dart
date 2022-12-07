aafb7583cf83d1586fd1f6120a9c11991e827427
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-07 10:29:53 -0100

    Test Dynamic text size (line 86, main.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
-      title: Text('Despesas Pessoais'),
+      title: Text(
+        'Despesas Pessoais',
+        style: TextStyle(
+          fontSize: 20 * MediaQuery.of(context).textScaleFactor,
+        ),
+      ),
       actions: [
         IconButton(
           icon: Icon(Icons.add),
